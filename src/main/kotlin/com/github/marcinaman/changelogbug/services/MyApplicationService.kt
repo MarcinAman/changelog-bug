d152b254b5b75da4f61093e737231d14a05394d3
GitHub Action <action@github.com>
2023-01-04 10:08:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/marcinaman/changelogbug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/marcinaman/changelogbug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.marcinaman.changelogbug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marcinaman.changelogbug.MyBundle
 
 class MyApplicationService {
 
